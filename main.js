356f5431e7c627bce41c30282b5abf601b5f8587
hahmsongmin <78192018+hahmsongmin@users.noreply.github.com>
2021-09-12 20:30:10 +0900

    click play and stop

--- main.js
+++ main.js
@@ -3,13 +3,18 @@
 const btnStart = document.querySelector(".btn-start");
 const btnStartIcon = btnStart.querySelector("i");
 
-const clickGameStart = () => {
+const clickGameStart = (event) => {
+  console.log(event);
   const audioBg = document.createElement("audio");
+  const audioTagC = audioBg.querySelector("[src='sound/bg.mp3']");
+  if (audioTagC) {
+    return;
+  }
   audioBg.setAttribute("src", "sound/bg.mp3");
   audioBg.setAttribute("autoplay", "");
   document.body.appendChild(audioBg);
 
-  btnStartIcon.contains("");
+  btnStartIcon.classList.toggle("fa-stop");
 };
 
 btnStart.addEventListener("click", clickGameStart);
